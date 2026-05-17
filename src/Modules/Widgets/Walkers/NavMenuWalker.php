8150572589643078a760e62071e720fee49398a3
Md. Ashraful <ashraful.coder@gmail.com>
2026-05-17 11:28:16 +0600

    Improve nav menu widget

--- src/Modules/Widgets/Walkers/NavMenuWalker.php
+++ src/Modules/Widgets/Walkers/NavMenuWalker.php
@@ -17,151 +17,183 @@ class NavMenuWalker extends Walker_Nav_Menu
     /**
      * Starts the list before the elements are added.
      *
-     * @param string   $output Used to append additional content (passed by reference).
-     * @param int      $depth  Depth of menu item. Used for padding.
+     * @param string    $output Used to append additional content (passed by reference).
+     * @param int       $depth  Depth of menu item. Used for padding.
      * @param \stdClass $args   An object of wp_nav_menu() arguments.
      */
     public function start_lvl(&$output, $depth = 0, $args = null): void
     {
-        if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
-            $t = '';
-            $n = '';
-        } else {
-            $t = "\t";
-            $n = "\n";
-        }
-        $indent = str_repeat($t, $depth);
-        $classes = ['elemacy-nav__submenu', 'sub-menu'];
-        $class_names = implode(' ', apply_filters('nav_menu_submenu_css_class', $classes, $args, $depth));
-        $output .= "{$n}{$indent}<ul class=\"" . esc_attr($class_names) . "\">{$n}";
+        [$tab, $newline] = $this->getSpacingCharacters($args);
+
+        $indent           = str_repeat($tab, $depth);
+        $submenu_classes  = ['elemacy-nav__submenu', 'sub-menu'];
+        $filtered_classes = apply_filters('nav_menu_submenu_css_class', $submenu_classes, $args, $depth);
+        $class_attribute  = esc_attr(implode(' ', $filtered_classes));
+
+        $output .= "{$newline}{$indent}<ul class=\"{$class_attribute}\">{$newline}";
     }
 
     /**
-     * Ends the list of after the elements are added.
+     * Ends the list after the elements are added.
      *
-     * @param string   $output Used to append additional content (passed by reference).
-     * @param int      $depth  Depth of menu item. Used for padding.
+     * @param string    $output Used to append additional content (passed by reference).
+     * @param int       $depth  Depth of menu item. Used for padding.
      * @param \stdClass $args   An object of wp_nav_menu() arguments.
      */
     public function end_lvl(&$output, $depth = 0, $args = null): void
     {
-        if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
-            $t = '';
-            $n = '';
-        } else {
-            $t = "\t";
-            $n = "\n";
-        }
-        $indent = str_repeat($t, $depth);
-        $output .= "{$indent}</ul>{$n}";
+        [$tab, $newline] = $this->getSpacingCharacters($args);
+
+        $indent  = str_repeat($tab, $depth);
+        $output .= "{$indent}</ul>{$newline}";
     }
 
     /**
      * Starts the element output.
      *
-     * @param string   $output            Used to append additional content (passed by reference).
-     * @param \WP_Post $data_object       Menu item data object.
-     * @param int      $depth             Depth of menu item. Used for padding.
+     * @param string    $output            Used to append additional content (passed by reference).
+     * @param \WP_Post  $data_object       Menu item data object.
+     * @param int       $depth             Depth of menu item. Used for padding.
      * @param \stdClass $args              An object of wp_nav_menu() arguments.
-     * @param int      $current_object_id Optional. ID of the current menu item. Default 0.
+     * @param int       $current_object_id Optional. ID of the current menu item. Default 0.
      */
     public function start_el(&$output, $data_object, $depth = 0, $args = null, $current_object_id = 0): void
     {
-        $args = (object) $args;
-        $item = $data_object;
+        $args      = (object) $args;
+        $menu_item = $data_object;
 
-        if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
-            $t = '';
-            $n = '';
-        } else {
-            $t = "\t";
-            $n = "\n";
-        }
-        $indent = ($depth) ? str_repeat($t, $depth) : '';
+        [$tab]  = $this->getSpacingCharacters($args);
+        $indent = $depth ? str_repeat($tab, $depth) : '';
 
-        $classes = empty($item->classes) ? [] : (array) $item->classes;
-        $classes[] = 'elemacy-nav__item';
-        $classes[] = 'menu-item-' . $item->ID;
+        $item_classes    = $this->buildListItemClasses($menu_item, $depth);
+        $class_names     = implode(' ', apply_filters('nav_menu_css_class', array_filter($item_classes), $menu_item, $args, $depth));
+        $is_current_item = in_array('current-menu-item', $item_classes, true);
+        $has_children    = in_array('menu-item-has-children', $item_classes, true);
 
-        if ($depth) {
-            $classes[] = 'elemacy-nav__item--depth-' . (int) $depth;
-        }
-        if (in_array('menu-item-has-children', $classes, true)) {
-            $classes[] = 'elemacy-nav__item--has-children';
-        }
+        $list_item_attributes        = ['class' => $class_names];
+        $list_item_attributes        = apply_filters('nav_menu_item_attributes', $list_item_attributes, $menu_item, $args, $depth);
+        $list_item_attribute_string  = $this->buildAttributeString($list_item_attributes, escape_urls: false);
 
-        $class_names = implode(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args, $depth));
+        $output .= $indent . '<li' . $list_item_attribute_string . '>';
 
-        $li_attrs = [];
-        $li_attrs['class'] = $class_names;
-        $li_attrs = apply_filters('nav_menu_item_attributes', $li_attrs, $item, $args, $depth);
+        $link_attributes        = $this->buildLinkAttributes($menu_item, $is_current_item, $has_children);
+        $link_attributes        = apply_filters('nav_menu_link_attributes', $link_attributes, $menu_item, $args, $depth);
+        $link_attribute_string  = $this->buildAttributeString($link_attributes, escape_urls: true);
 
-        $attributes = '';
-        foreach ($li_attrs as $attr => $value) {
-            if (!empty($value)) {
-                $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
-                $attributes .= ' ' . $attr . '="' . $value . '"';
-            }
-        }
+        $title = apply_filters('the_title', $menu_item->title, $menu_item->ID);
+        $title = apply_filters('nav_menu_item_title', $title, $menu_item, $args, $depth);
 
-        $output .= $indent . '<li' . $attributes . '>';
-
-        $link_classes = ['elemacy-nav__link'];
-        if (in_array('current-menu-item', $classes, true)) {
-            $link_classes[] = 'elemacy-is-active';
-        }
-
-        $atts = [];
-        $atts['title'] = !empty($item->attr_title) ? $item->attr_title : '';
-        $atts['target'] = !empty($item->target) ? $item->target : '';
-        if ('_blank' === $item->target && empty($item->xfn)) {
-            $atts['rel'] = 'noopener';
-        } else {
-            $atts['rel'] = $item->xfn;
-        }
-        $atts['href'] = !empty($item->url) ? $item->url : '';
-        $atts['class'] = implode(' ', $link_classes);
-        $atts['aria-current'] = in_array('current-menu-item', $classes, true) ? 'page' : '';
-
-        $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args, $depth);
-
-        $attributes = '';
-        foreach ($atts as $attr => $value) {
-            if (is_scalar($value) && '' !== $value && false !== $value) {
-                $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
-                $attributes .= ' ' . $attr . '="' . $value . '"';
-            }
-        }
-
-        $title = apply_filters('the_title', $item->title, $item->ID);
-        $title = apply_filters('nav_menu_item_title', $title, $item, $args, $depth);
-
-        $item_output = $args->before ?? '';
-        $item_output .= '<a' . $attributes . '>';
+        $item_output  = $args->before ?? '';
+        $item_output .= '<a' . $link_attribute_string . '>';
         $item_output .= ($args->link_before ?? '') . $title . ($args->link_after ?? '');
         $item_output .= '</a>';
         $item_output .= $args->after ?? '';
 
-        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $menu_item, $depth, $args);
     }
 
     /**
      * Ends the element output.
      *
-     * @param string   $output      Used to append additional content (passed by reference).
-     * @param \WP_Post $data_object Menu item data object.
-     * @param int      $depth       Depth of menu item. Used for padding.
+     * @param string    $output      Used to append additional content (passed by reference).
+     * @param \WP_Post  $data_object Menu item data object.
+     * @param int       $depth       Depth of menu item. Used for padding.
      * @param \stdClass $args        An object of wp_nav_menu() arguments.
      */
     public function end_el(&$output, $data_object, $depth = 0, $args = null): void
     {
+        [, $newline] = $this->getSpacingCharacters($args);
+        $output     .= "</li>{$newline}";
+    }
+
+    /**
+     * Returns the tab and newline characters based on the item_spacing argument.
+     *
+     * @param \stdClass|null $args
+     * @return array{string, string} [$tab, $newline]
+     */
+    private function getSpacingCharacters($args): array
+    {
         if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
-            $t = '';
-            $n = '';
-        } else {
-            $t = "\t";
-            $n = "\n";
+            return ['', ''];
         }
-        $output .= "</li>{$n}";
+
+        return ["\t", "\n"];
+    }
+
+    /**
+     * Builds the CSS class list for a list item element.
+     *
+     * @param \WP_Post $menu_item
+     * @param int      $depth
+     * @return array<string>
+     */
+    private function buildListItemClasses($menu_item, int $depth): array
+    {
+        $classes   = empty($menu_item->classes) ? [] : (array) $menu_item->classes;
+        $classes[] = 'elemacy-nav__item';
+        $classes[] = 'menu-item-' . $menu_item->ID;
+
+        if ($depth) {
+            $classes[] = 'elemacy-nav__item--depth-' . $depth;
+        }
+
+        if (in_array('menu-item-has-children', $classes, true)) {
+            $classes[] = 'elemacy-nav__item--has-children';
+        }
+
+        return $classes;
+    }
+
+    /**
+     * Builds the anchor tag attributes for a menu item link.
+     *
+     * @param \WP_Post $menu_item
+     * @param bool     $is_current_item
+     * @param bool     $has_children
+     * @return array<string, string>
+     */
+    private function buildLinkAttributes($menu_item, bool $is_current_item, bool $has_children): array
+    {
+        $link_classes   = ['elemacy-nav__link'];
+        $link_classes[] = $is_current_item ? 'elemacy-is-active' : '';
+
+        $attributes = [
+            'title'        => $menu_item->attr_title ?? '',
+            'target'       => $menu_item->target ?? '',
+            'rel'          => ('_blank' === $menu_item->target && empty($menu_item->xfn)) ? 'noopener' : ($menu_item->xfn ?? ''),
+            'href'         => $menu_item->url ?? '',
+            'class'        => implode(' ', array_filter($link_classes)),
+            'aria-current' => $is_current_item ? 'page' : '',
+        ];
+
+        if ($has_children) {
+            $attributes['aria-haspopup'] = 'menu';
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Converts an associative array of attributes into an HTML attribute string.
+     *
+     * @param array<string, mixed> $attributes
+     * @param bool                 $escape_urls  Whether to apply esc_url() to href values.
+     * @return string
+     */
+    private function buildAttributeString(array $attributes, bool $escape_urls = true): string
+    {
+        $attribute_string = '';
+
+        foreach ($attributes as $attribute_name => $attribute_value) {
+            if (!is_scalar($attribute_value) || '' === $attribute_value || false === $attribute_value) {
+                continue;
+            }
+
+            $escaped_value     = ($escape_urls && 'href' === $attribute_name) ? esc_url($attribute_value) : esc_attr($attribute_value);
+            $attribute_string .= ' ' . $attribute_name . '="' . $escaped_value . '"';
+        }
+
+        return $attribute_string;
     }
 }
--- src/Modules/Widgets/assets/scripts/nav-menu.js
+++ src/Modules/Widgets/assets/scripts/nav-menu.js
@@ -1,53 +1,97 @@
 (function ($) {
     "use strict";
 
-    const ElemacyNavHandler = function ($scope, $) {
-        // Scope the elements to this specific widget instance only
-        const $nav = $scope.find('.elemacy-nav');
+    const ElemacyNavHandler = function ($scope) {
+        const $nav    = $scope.find('.elemacy-nav');
         const $toggle = $scope.find('.elemacy-nav__toggle, .elemacy-nav__toggle-close');
-        const $menu = $scope.find('.elemacy-nav__menu');
 
-        // Toggle Click Event
+        // ── Close helper ─────────────────────────────────────────────────────
+        // Keeps .is-open on the nav while the overlay fades/slides out so that
+        // the :not(.is-open) desktop selectors never fire during the transition.
+        let closeTimer;
+
+        function closeMenu() {
+            clearTimeout(closeTimer);
+            $nav.addClass('elemacy-nav--closing');
+            closeTimer = setTimeout(function () {
+                $nav.removeClass('is-open elemacy-nav--closing');
+                $toggle.attr('aria-expanded', 'false');
+            }, 300); // must match CSS transition-duration
+        }
+
+        // ── Open ─────────────────────────────────────────────────────────────
+        function openMenu() {
+            clearTimeout(closeTimer);
+            $nav.removeClass('elemacy-nav--closing').addClass('is-open');
+            $toggle.attr('aria-expanded', 'true');
+        }
+
+        // ── Toggle button ────────────────────────────────────────────────────
         $toggle.on('click', function (e) {
             e.preventDefault();
-
-            const isOpen = $nav.hasClass('is-open');
-
-            // Toggle the class on the main wrapper
-            $nav.toggleClass('is-open');
-
-            // Accessibility: Update ARIA states
-            $toggle.attr('aria-expanded', !isOpen);
+            $nav.hasClass('is-open') ? closeMenu() : openMenu();
         });
 
-        // Close menu if clicking outside (Optional but recommended for UX)
-        $(document).on('click', function (e) {
-            if ($nav.hasClass('is-open') && !$nav.is(e.target) && $nav.has(e.target).length === 0) {
-                $nav.removeClass('is-open');
-                $toggle.attr('aria-expanded', 'false');
+        // ── Backdrop click closes slide panel overlays ────────────────────────
+        $scope.find('.elemacy-nav__backdrop').on('click', function () {
+            if ($nav.hasClass('is-open')) {
+                closeMenu();
             }
         });
 
-        // Submenu collision detection to prevent overflow
-        const $menuItems = $scope.find('.elemacy-nav__item');
-        $menuItems.on('mouseenter', function () {
+        // ── Fullscreen overlay: click directly on backdrop (the menu box) ────
+        $scope.find('.elemacy-nav__menu').on('click', function (e) {
+            if ($nav.hasClass('is-open') && e.target === this) {
+                closeMenu();
+            }
+        });
+
+        // ── Close on outside click (handles edge cases) ──────────────────────
+        $(document).on('click.elemacy-nav-' + $scope.data('id'), function (e) {
+            if (
+                $nav.hasClass('is-open') &&
+                !$nav.is(e.target) &&
+                $nav.has(e.target).length === 0
+            ) {
+                closeMenu();
+            }
+        });
+
+        // ── Close on Escape ──────────────────────────────────────────────────
+        $(document).on('keydown.elemacy-nav-' + $scope.data('id'), function (e) {
+            if (e.key === 'Escape' && $nav.hasClass('is-open')) {
+                closeMenu();
+                $scope.find('.elemacy-nav__toggle').first().trigger('focus');
+            }
+        });
+
+        // ── Submenu collision detection ──────────────────────────────────────
+        // The submenu is display:none when hidden, so getBoundingClientRect()
+        // returns all zeros. Briefly render it invisible to measure its bounds,
+        // apply overflow classes, then let CSS :hover reveal it.
+        $scope.find('.elemacy-nav__item--has-children').on('mouseenter', function () {
             const $submenu = $(this).children('.elemacy-nav__submenu');
-            if ($submenu.length) {
-                // Reset class to measure natural position
-                $submenu.removeClass('elemacy-is-drop-left');
-                const rect = $submenu[0].getBoundingClientRect();
-                const windowWidth = $(window).width() || document.documentElement.clientWidth;
+            if (!$submenu.length) return;
 
-                // Add class if it overflows the right edge
-                if (rect.right > windowWidth) {
-                    $submenu.addClass('elemacy-is-drop-left');
-                }
-            }
+            $submenu.removeClass('elemacy-is-drop-left elemacy-is-drop-up');
+
+            $submenu.css({ display: 'flex', visibility: 'hidden', animation: 'none', transition: 'none' });
+            const rect = $submenu[0].getBoundingClientRect();
+            $submenu.css({ display: '', visibility: '', animation: '', transition: '' });
+
+            const winW = window.innerWidth  || document.documentElement.clientWidth;
+            const winH = window.innerHeight || document.documentElement.clientHeight;
+
+            if (rect.right  > winW) $submenu.addClass('elemacy-is-drop-left');
+            if (rect.bottom > winH) $submenu.addClass('elemacy-is-drop-up');
         });
     };
 
     $(window).on('elementor/frontend/init', function () {
-        elementorFrontend.hooks.addAction('frontend/element_ready/elemacy_nav_menu.default', ElemacyNavHandler);
+        elementorFrontend.hooks.addAction(
+            'frontend/element_ready/elemacy_nav_menu.default',
+            ElemacyNavHandler
+        );
     });
 
 })(jQuery);
