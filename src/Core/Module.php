7ffe362dc224bea961c99df5a3aa04ff2f83d1ad
Md. Ashraful <ashraful.coder@gmail.com>
2026-06-06 22:36:15 +0600

    Refactor Module class

--- src/Core/Module.php
+++ src/Core/Module.php
@@ -4,8 +4,6 @@ namespace Elemacy\Core;
 
 defined('ABSPATH') || exit;
 
-use Elemacy\Core\Constants\OptionKeys;
-
 abstract class Module
 {
 	abstract public function get_name(): string;
@@ -25,13 +23,6 @@ abstract class Module
 	public function on_enable(): void {}
 
 	public function on_disable(): void {}
-
-	public function is_active(): bool
-	{
-		$active_modules = get_option(OptionKeys::ACTIVE_MODULES, []);
-
-		return in_array($this->get_name(), $active_modules, true);
-	}
 
 	public function is_always_active(): bool
 	{
@@ -56,23 +47,5 @@ abstract class Module
 	public function is_mock(): bool
 	{
 		return false;
-	}
-
-	protected function get_option(string $key, $default_value = null)
-	{
-		$option_key = 'elemacy_' . $this->get_name() . '_' . $key;
-		return get_option($option_key, $default_value);
-	}
-
-	protected function update_option(string $key, $value): bool
-	{
-		$option_key = 'elemacy_' . $this->get_name() . '_' . $key;
-		return update_option($option_key, $value);
-	}
-
-	protected function delete_option(string $key): bool
-	{
-		$option_key = 'elemacy_' . $this->get_name() . '_' . $key;
-		return delete_option($option_key);
 	}
 }
